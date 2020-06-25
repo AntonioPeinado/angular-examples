30d3563119503170653343715ce5b7c44715a884
Isidro Torregrosa <isistorre95@gmail.com>
2020-06-25 10:33:24 +0200

    directives y pipes

--- src/app/examples/examples.module.ts
+++ src/app/examples/examples.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { TemplatesModule } from './templates/templates.module';
 import { DataModule } from './data/data.module';
+import { PipesModule } from './pipes/pipes.module';
+import { DirectivesModule } from './directives/directives.module';
 
 @NgModule({
   exports: [
     TemplatesModule,
-    DataModule
+    DataModule,
+    PipesModule,
+    DirectivesModule
   ]
 })
 export class ExamplesModule {}
